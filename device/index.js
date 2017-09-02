52028dda161c2beab29d7fa4a8b4a9355d10bebc
iwanaga <iwanaga.blackie@gmail.com>
2017-09-02 17:05:03 +0000

    add all buttons

--- device/index.js
+++ device/index.js
@@ -7,13 +7,23 @@ let board = new j5.Board({
 });
 
 board.on('ready', () => {
-    let led = new j5.Led('P1-7');
-    let button = new j5.Button('P1-11');
+    let led          = new    j5.Led('P1-3');
+    let redButton    = new j5.Button('P1-5');
+    let yellowButton = new j5.Button('P1-7');
+    let greenButton  = new j5.Button('P1-11');
+    let blueButton   = new j5.Button('P1-13');
+    let purlpeButton = new j5.Button('P1-15');
+    let blackButton  = new j5.Button('P1-19');
 
-    button.on('down', () => {
-        console.log('button pushed');
+    redButton.on('down', () => {
+        console.log('RED  pushed');
         feedBackByLed(led);
     });
+    yellowButton.on('down', () => {
+        console.log('YELLOW pushed');
+        feedBackByLed(led);
+    });
+
 });
 
 function feedBackByLed(led) {
