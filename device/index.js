06f08f58237f419205b975240aa280070e8acf10
Yoshihiro Iwanaga <iwanaga.blackie@gmail.com>
2017-09-02 15:34:29 +0900

    test

---
+++ device/index.js
@@ -0,0 +1,23 @@
+const j5 = require('johnny-five');
+const RaspPiIo = require('raspi-io');
+let board = new j5.Board({
+    io: new RaspPiIo()
+});
+
+board.on('ready', () => {
+    let led = new j5.Led('P1-4');
+    let button = new j5.Button('P1-13');
+
+    button.on('down', () => {
+        console.log('button pushed');
+        feedBackByLed(led);
+    });
+});
+
+function feedBackByLed(led) {
+    led.strobe(100);
+    setTimeout(() => {
+        led.stop();
+        led.off();
+    }, 1000);
+}
